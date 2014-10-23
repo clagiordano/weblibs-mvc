0939366825a96531004259270a3ef6f712dec285
Claudio Giordano <claudio.giordano@autistici.org>
2014-10-24 00:11:01 +0200

    fix accesso alle variabili del template

--- controllers/error404.php
+++ controllers/error404.php
@@ -6,7 +6,8 @@ class error404Controller Extends Controller {
 
     public function index() 
     {
-        $this->registry->template->heading = 'This is the 404';
+        $this->registry->template->heading = "Oops, you've found a dead link.";
+        $this->registry->template->sub_heading = "Use the links at the top to get back.";
         $this->registry->template->show('error404');
     }
 }
--- views/error404.php
+++ views/error404.php
@@ -1,3 +1,14 @@
-<h1><?php echo $heading; ?></h1>
+<?php
+//    print_r(__FILE__);
+//    
+//    echo "<pre>";
+//        print_r($this->registry->template->vars['heading']);
+//    echo "</pre>";
+?>
+
+
+
+<h1><?php echo $this->registry->template->vars['heading']; ?></h1>
+<h3><?php echo $this->registry->template->vars['sub_heading']; ?></h3>
 <p>This is a custom 404 error page.</p>
 <p>You can put whatever content you like here such as search for your site</p>
