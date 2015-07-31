0ce4d4834518e8d864dc9b4710ce7cf711806f9c
Claudio Giordano <claudio.giordano@autistici.org>
2015-07-31 22:21:00 +0200

    various fix

--- includes/init.php
+++ includes/init.php
@@ -5,18 +5,18 @@
  *
  * @param string $className
  */
-//function __autoload($className)
-//{
-//    $className = ltrim($className, '\\');
-//    $fileName  = '';
-//    $namespace = '';
-//    if ($lastNsPos = strrpos($className, '\\')) {
-//        $namespace = substr($className, 0, $lastNsPos);
-//        $className = substr($className, $lastNsPos + 1);
-//        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-//    }
-//    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-//
-//    //print "psr autoload: $fileName<br />";
-//    require "../$fileName";
-//}
+function __autoload($className)
+{
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    //print "psr autoload: $fileName<br />";
+    require "../$fileName";
+}
