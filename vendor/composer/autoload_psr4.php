d335da4c9901c464f0ef3fa1349341b83022be3f
Claudio Giordano <claudio.giordano@autistici.org>
2015-07-31 22:34:04 +0200

    fix composer.json and regen autoload psr4

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'clagiordano\\webmvc' => array($baseDir . '/application'),
+    'clagiordano\\webmvc\\' => array($baseDir . '/application'),
 );
