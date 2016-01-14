3ec0bd010990725dda5f857e16aeb9287aafa9a5
Claudio Giordano <claudio.giordano@autistici.org>
2016-01-14 21:01:53 +0100

    Removed commented code

--- views/error404.php
+++ views/error404.php
@@ -1,13 +1,3 @@
-<?php
-//    print_r(__FILE__);
-//    
-//    echo "<pre>";
-//        print_r($this->registry->template->vars['heading']);
-//    echo "</pre>";
-?>
-
-
-
 <h1><?php echo $this->registry->template->vars['heading']; ?></h1>
 <h3><?php echo $this->registry->template->vars['sub_heading']; ?></h3>
 <p>This is a custom 404 error page.</p>
--- views/index.php
+++ views/index.php
@@ -1,9 +1,3 @@
-<?php
-
-//echo __FILE__; 
-
-?>
-
 <h1>
     <?php echo $this->registry->template->vars['welcome']; ?>
 </h1>
