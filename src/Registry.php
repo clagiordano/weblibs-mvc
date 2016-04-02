453e787210c369796980b7fd472ea20756e6c19b
Claudio Giordano <claudio.giordano@autistici.org>
2016-04-02 18:42:56 +0200

    Revert registry

--- src/Registry.php
+++ src/Registry.php
@@ -13,20 +13,6 @@ namespace clagiordano\weblibs\mvc;
  */
 class Registry
 {
-    /** @var Controller $controller **/
-    private $controller = null;
-    /** @var Registry $registry */
-    private $registry = null;
-    /** @var Router $router **/
-    private $router = null;
-    /** @var Template $template **/
-    private $template = null;
-
-    /**
-     * @var array $vars
-     */
-    private $vars = [];
-
     /**
      * @return clagiordano\weblibs\mvc\Registry
      */
@@ -36,61 +22,10 @@ class Registry
     }
 
     /**
-     * @param Controller $controller
-     * @return \clagiordano\weblibs\mvc\Application
+     * @the vars array
+     * @access private
      */
-    public function setController(Controller $controller)
-    {
-        $this->controller = $controller;
-
-        return $this;
-    }
-
-    /**
-     * @return \clagiordano\weblibs\mvc\Router
-     */
-    public function getController()
-    {
-        return $this->controller;
-    }
-
-    /**
-     * @param Router $router
-     * @return \clagiordano\weblibs\mvc\Application
-     */
-    public function setRouter(Router $router)
-    {
-        $this->router = $router;
-
-        return $this;
-    }
-
-    /**
-     * @return \clagiordano\weblibs\mvc\Router
-     */
-    public function getRouter()
-    {
-        return $this->router;
-    }
-
-    /**
-     * @param Template $template
-     * @return \clagiordano\weblibs\mvc\Application
-     */
-    public function setTemplate(Template $template)
-    {
-        $this->template = $template;
-
-        return $this;
-    }
-
-    /**
-     * @return \clagiordano\weblibs\mvc\Template
-     */
-    public function getTemplate()
-    {
-        return $this->template;
-    }
+    private $vars = [];
 
     /**
      * @set undefined vars
