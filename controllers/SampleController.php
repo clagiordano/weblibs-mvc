b354cbfcf209b82b0b450edb0c2d0387085047b0
Claudio Giordano <claudio.giordano@autistici.org>
2016-04-15 00:22:28 +0200

    Fix routing issues

---
+++ controllers/SampleController.php
@@ -0,0 +1,20 @@
+<?php
+
+use clagiordano\weblibs\mvc\Controller;
+
+/**
+ * Sample default IndexController
+ */
+class SampleController extends Controller
+{
+    /**
+     * All controllers must implements an index method
+     */
+    public function index()
+    {
+        /** set a template variable */
+        $this->application->getRegistry()->header = 'This is the sample controller';
+        /** load the index template */
+        $this->application->getTemplate()->show('index');
+    }
+}
